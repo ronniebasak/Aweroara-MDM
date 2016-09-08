accc43e60bb2ff6614dadedb8c290232ceec8f80
= <ronnie.basak96@gmail.com>
2016-09-09 00:06:30 +0530

    Added setup.sh for easy set up

---
+++ setup.sh
@@ -0,0 +1,6 @@
+
+sudo mkdir /usr/share/mdm/html-themes/Awerora
+sudo cp -R * /usr/share/mdm/html-themes/Awerora
+
+echo "Please Select Aurora in the following window to set this theme up"
+sudo mdmsetup
